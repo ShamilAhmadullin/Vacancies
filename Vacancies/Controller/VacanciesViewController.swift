387f1637cd1f4e8a9a869d96e022b5f2bce2a58f
Shamil Ahmadullin <shbravah@gmail.com>
2019-02-21 23:03:41 +0300

    Add activity indicator and auto scroll
    Add activity indicator during requests.
    Implemented auto scroll to top row if search new profession.

--- Vacancies/Controller/VacanciesViewController.swift
+++ Vacancies/Controller/VacanciesViewController.swift
@@ -13,11 +13,14 @@ final class VacanciesViewController: UIViewController {
     
     @IBOutlet weak private var vacanciesTableView: UITableView!
     @IBOutlet weak var professionSearchBar: UISearchBar!
+    @IBOutlet weak var activityIndicatorView: UIActivityIndicatorView!
     
     private var vacancies: [Vacancy] = []
     private var searchProfession = "java"
     private var page = 1
     private var selectedRow = 0
+    private var isThereAreVacancies = true
+    private var isSearchProfessionChanged = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,9 +44,19 @@ final class VacanciesViewController: UIViewController {
     private func getVacancies(search: String, page: Int) {
         Core.getVacancies(VacanciesParameters(search: search, page: page).parameters) { [weak self] vacancies in
             guard let self = self else { return }
-            self.vacancies += vacancies
+            if vacancies.count == 0 {
+                self.isThereAreVacancies.toggle()
+            } else {
+               self.vacancies += vacancies
+            }
             DispatchQueue.main.async {
+                self.activityIndicatorView.stopAnimating()
+                self.activityIndicatorView.isHidden = true
                 self.vacanciesTableView.reloadData()
+                if self.isSearchProfessionChanged {
+                    self.isSearchProfessionChanged.toggle()
+                    self.vacanciesTableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: .top, animated: true)
+                }
             }
         }
     }
@@ -92,8 +105,16 @@ extension VacanciesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
         let lastItem = vacancies.count - 1
+        if isThereAreVacancies {
+            checkLastItemAndGetVacancies(lastItem, indexPath)
+        }
+    }
+    
+    private func checkLastItemAndGetVacancies(_ lastItem: Int, _ indexPath: IndexPath) {
         if lastItem == indexPath.row {
             page += 1
+            activityIndicatorView.isHidden = false
+            activityIndicatorView.startAnimating()
             getVacancies(search: searchProfession, page: page)
         }
     }
@@ -108,6 +129,10 @@ extension VacanciesViewController: UISearchBarDelegate {
             searchProfession = profession
             page = 1
             vacancies = []
+            isThereAreVacancies = true
+            isSearchProfessionChanged = true
+            activityIndicatorView.isHidden = false
+            activityIndicatorView.startAnimating()
             getVacancies(search: searchProfession, page: page)
         }
         view.endEditing(true)
